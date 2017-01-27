2220017be57aecf61919fe35108f7481bcab1e49
Kyle Kastner <kastnerkyle@gmail.com>
2017-01-27 18:07:27 -0500

    Work in progress

--- examples/shared/run_sbatch.sh
+++ examples/shared/run_sbatch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 qos=${2:-unkillable}
 gpu=${3:-titanx}
-sbatch -o "slurm-%j_$1.out" --comment "$1" ./run_slurm.sh $1 $2 $3
+
+sbatch -o "slurm-%j_$1.out" --comment "$1" --get-user-env --gres=gpu:$gpu --qos=$qos --mem=15999 --no-requeue ./run_slurm.sh $1 $2 $3
--- examples/shared/run_slurm.sh
+++ examples/shared/run_slurm.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 source /u/kastner/.bashrc
-export THEANO_FLAGS="floatX=float32,device=gpu,force_device=True,lib.cnmem=1"
+export THEANO_FLAGS="floatX=float32,device=gpu0,force_device=True,lib.cnmem=1"
 export PYTHONUNBUFFERED=1
 echo Running: python "$1" on `hostname`
-python $1
+python -u $1
 qos=${2:-unkillable}
 gpu=${3:-titanx}
 echo Using gpu "$gpu" with qos "$qos"
-sbatch --get-user-env --gres=gpu:$gpu --qos=$qos --mem=15999 $1
